aa260d6d148deadc2ea0073e00d128b153369d71
Justynn <jhamm2315@gmail.com>
2026-02-13 16:41:04 -0700

    Apply local changes after rebase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -26,7 +26,7 @@ function mount() {
 
     ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </React.StrictMode>
